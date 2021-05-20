778ff8f0bac9a51302865ba46a6e756e97e07b57
Luis Enrique Fuentes Plata <domfp13@me.com>
2021-05-20 22:05:32 +0200

    Adding few tests for the GenericFunctions.py

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -1,6 +1,9 @@
 import json, pytest
+#from transformation.app import lambda_handler
 
-from transformation import lambda_handler
+from transformation.src.GenericFunctions import (
+    get_path, get_client
+)
 
 @pytest.fixture()
 def s3_event():
@@ -35,7 +38,7 @@ def s3_event():
                     "arn": "arn:aws:s3:::domfp13-s3-bucket"
                 },
                 "object": {
-                    "key": "customer-a/TestingFile.txt",
+                    "key": "customer-a/Allegis_test.csv",
                     "size": 1024,
                     "eTag": "0123456789abcdef0123456789abcdef",
                     "sequencer": "0A1B2C3D4E5F678901"
@@ -45,12 +48,24 @@ def s3_event():
             ]
         }
 
-def test_lambda_handler(s3_event, mocker):
+# def test_lambda_handler(s3_event, mocker):
+#     ret = app.lambda_handler(apigw_event, "")
+#     data = json.loads(ret["body"])
 
-    ret = app.lambda_handler(apigw_event, "")
-    data = json.loads(ret["body"])
-
-    assert ret["statusCode"] == 200
-    assert "message" in ret["body"]
-    assert data["message"] == "hello world"
+#     assert ret["statusCode"] == 200
+#     assert "message" in ret["body"]
+#     assert data["message"] == "hello world"
     # assert "location" in data.dict_keys()
+
+def test_get_path():
+    """Testing if the function if returning the same type of object
+    """
+    from pathlib import Path
+    assert type(get_path('Allegis_test.csv')) == type(Path())
+
+def test_get_client():
+    """Testing if the function if returning the same type of object
+    """
+    import boto3_type as bt
+    client = get_client()
+    assert bt.client.istype(client, "s3")
